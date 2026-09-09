4a565aebe277852792e2a2d83c20e511b1f4f59f
.\.git <.\.gitignore>
2026-09-08 23:53:09 -0300

    please work

--- routes/contacts.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const contactsController = require('../controllers/contacts');
-
-router.get('/', contactsController.getAllContacts);
-router.get('/:id', contactsController.getSingleContact);
-
-module.exports = router;
